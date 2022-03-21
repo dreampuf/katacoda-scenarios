508f8c050572c97c9b1dc5e8ebbd1cfcaa075d4e
Xin Huang <soddyque@gmail.com>
2022-03-22 01:10:44 +0800

    Update mdl

--- mysql-sandbox/foreground.sh
+++ mysql-sandbox/foreground.sh
@@ -31,7 +31,7 @@ mysql --user admin < sakila-db/sakila-data.sql
 cd $DIR
 echo "Environment is setup and ready to go"
 
-mysql --user=admin
+#mysql --user=admin
 
 echo "The IP address for this environment is [[HOST_IP]]"
 echo "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
