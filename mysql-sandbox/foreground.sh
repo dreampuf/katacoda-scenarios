4ff252e63fda5b6cd17c2e3ad778f7edd62a0f18
Xin Huang <soddyque@gmail.com>
2022-03-22 15:15:45 +0800

    Update mysql-sandbox

--- mysql-sandbox/foreground.sh
+++ mysql-sandbox/foreground.sh
@@ -39,7 +39,6 @@ echo "The IP address for this environment is [[HOST_IP]]"
 echo "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
 mysql --user=admin
 
-SET GLOBAL log_bin = ON;
 SHOW BINARY LOGS;
 show databases;
 use sakila;
